8e8a5c1e4b63ba81c2a018deacdb23e729da1677
ngirinshuti <prudentenri001@gmail.com>
2021-10-15 15:43:22 +0200

    new files

--- login.php
+++ login.php
@@ -1,67 +1,37 @@
 <?php 
 session_start();
-if (!isset($_SESSION['pass'],$_SESSION['name'])) {
-  header("location:index.php");
+if(isset($_SESSION['unique_id'])){
+    header("location: users.php");
 }
-$name=$_SESSION['name'];
-$pwd=$_SESSION['pass'];
-?>
-<?php
-session_start();
-$name=$_POST['usern'];
-$pwd=$_POST['pass'];
-$salted="salting_string@12345".$pwd;
- $pass = sha1($salted);
-$x=0;
-$y=0;
-include("connect.php");
-$sql="select* from account where username=? and password=?";
-$stmt= mysqli_stmt_init($conn);
-if (!mysqli_stmt_prepare($stmt,$sql)) {
- echo "statement failed";
-}
-else{
-  mysqli_stmt_bind_param($stmt,"ss",$name,$pass);
-  mysqli_stmt_execute($stmt);
-  $select=mysqli_stmt_get_result($stmt);
-while($user=mysqli_fetch_array($select))
-{
-if(($name==$user['username'])&&($pass==$user['password']))
-{
- 
- $fst=$user['username'];
-  $eml=$user['email'];
-  $x=1;
-  $_SESSION['name']=$name;
-  $_SESSION['pass']=$pwd;
-  $status=$user['status'];
+ ?>
+<?php include_once "header.php"; ?>
+<body style="background-color: skyblue;">
+    <div class="wrapper">
+        <section class="form login">
+            <header>Login to our chat App</header>
+            <form action="#" autocomplete="off">
+                <div class="error-txt"></div>
 
-}
-}
-}
-if($x==1)
-{
-   if ($status=='Verified') {
-  if (!empty($_POST['remember'])) {
-$check=$_POST['remember'];
-   setcookie("name",$name,time()+3600*24*7);
-   setcookie("password",$pwd,time()+3600*24*7);
-   setcookie("check",$check,time()+3600*24*7);
-  }
-  else{
-$check=0;
-     setcookie("name",$name,7);
-       setcookie("password",$pwd,7);
-         setcookie("check",$check,7);
-  }
-header("location:dashboard.php");
-}
-else{
-  header("location:index.php?verfied=notverfied");
-}
-}
-else{
-  header("location:index.php?new=incorrect");
-}
+                <div class="field input">
+                    <label>Email Address</label>
+                    <input type="text" placeholder="Enter your email" name="email">
+                </div>
+                <div class="field input">
+                    <label>Password</label>
+                    <input type="password" placeholder="Enter your password" name="password">
+                    <i class="fas fa-eye"></i>
+                </div>
+                <div class="field button">
+                    <input type="submit" value="Continue to chat" name="">
+                </div>
 
-?>
+            </form>
+            <div class="link">Not yet signed up? <a href="index.php">signup now </a></div>
+        </section>
+    </div>
+
+    <script src="javascript/pass-show-hide.js"></script>
+    <script src="javascript/login.js"></script>
+
+</body>
+</html>
--- logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-session_destroy();
-setcookie("name","",-20);
-setcookie("password","",-20);
-header("location:index.php");
-
-?>
